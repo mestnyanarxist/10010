ac6bf4fbca22f7e2fe483c5b8e9ff4f7a8f849f8
Миха Новиков <rodgdestwo@yandex.ru>
2020-12-11 11:18:40 +0300

    Add Novicov file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Novicov from './containers/Novicov'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Novicov />
     </div>
   );
 }
